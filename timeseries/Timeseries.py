a898c5de16407027cd84ee40012934798d1252fe
Sarah Anoke <sanoke527@gmail.com>
2016-10-22 14:34:16 -0400

    Added reprlib to TimeSeries (#2) Changed the coding of dunders 'repr' and 'str' to delegate to reprlib functions for string representation truncation.

--- timeseries/Timeseries.py
+++ timeseries/Timeseries.py
@@ -1,6 +1,7 @@
 import lazy
 import numpy as np
 import numbers
+import reprlib
 
 class TimeSeries:
     
@@ -34,7 +35,6 @@ class TimeSeries:
            -
            -
         """
-
         if times == None:
             self._times = range(0,len(values))
         else:
@@ -48,12 +48,8 @@ class TimeSeries:
     
     
     def __getitem__(self, time):
-        """
-        Reports the data value located at `self[time]`, where
-            `self` is a TimeSeries instance.
-        Note: `time` is a list index, not an actual observed time
-            in the series.
-        """        
+        # reports the data value located at `self[time]`, where `self` is a TimeSeries instance.
+        # note: `time` is a list index, not an actual observed time in the series.
         try:
             return self._values[self._times.index(time)]
         except IndexError:
@@ -61,103 +57,84 @@ class TimeSeries:
         
     
     def __setitem__(self, time, value):
-        """
-        Note: `time` is a list index, not an actual observed time
-            in the series.
-        """         
+        # sets the data value located at `self[time]` to `value`, 
+        #     where `self` is a TimeSeries instance.
+        # note: `time` is a list index, not an actual observed time in the series.
         try:
             self._values[self._times.index(time)] = value
         except IndexError:
             raise("Index out of bounds.")
         
-    def __repr__(self):
-        """
-        Prints a string representation of a TimeSeries instance.
-        - For a TimeSeries instance with > 10 elements:
-             returns a string with name of the class, length of the instance, 
-             and list containing the first element and the last element 
-             separated by an ellipsis. 
-        - For a TimeSeries instance with <= 10 elements: 
-             returns a string with name of the class, length of the instance, 
-             and the full list representation of the data.        
-        """           
-        class_name = type(self).__name__
-        print(type(self))
-        if len(self._values) > 10:
-            if np.ndim(self._values)==1:
-                return '{}(Length: {}, Contents:[({},{}), ... ,({},{})])'.format(class_name, len(self._values), self._times[0], self._values[0], self._times[-1],self._values[-1]) 
-            else:
-                return '{}(Length: {}, Contents:[({}), ... ,({})])'.format(class_name, len(self._values), self._values[0], self._values[-1])                 
-        else:
-            if np.ndim(self._values)==1:               
-                return '{}(Length: {}, Contents:[{}])'.format(class_name, len(self._values), ','.join([str(z) for z in zip(self._times, self._values)]))      
-            else:
-                return '{}(Length: {}, Contents:[{}])'.format(class_name, len(self._values), ','.join([str(z) for z in self._values]))                      
+        
+    def __repr__(self):   
+        r = reprlib.Repr()
+        r.maxlist = 3       # max elements displayed for lists
+        cls = type(self).__name__
+        timesStr  = r.repr(self._times)
+        timesStr = r.repr(self._values)
+        return "{}(Length: {}, Times: {}, Values: {})".format(cls, len(self._values), timesStr, timesStr) 
+        
         
     def __str__(self):
-        """
-        Prints a string representation of a TimeSeries instance.
-        - For a TimeSeries instance with > 10 elements:
-             prints a string with a list containing the first element 
-             and the last element separated by an ellipsis. 
-        - For a TimeSeries instance with <= 10 elements: 
-             prints the full list representation of the data.        
-        """           
-        class_name = type(self).__name__
-        if len(self._values) > 10:
-            if np.ndim(self._values)==1:
-                return 'Length: {} [({},{}), ... ,({},{})]'.format(len(self._values), self._times[0], self._values[0], self._times[-1],self._values[-1]) 
-            else:
-                return 'Length: {} [({}), ... ,({})]'.format(len(self._values), self._values[0],self._values[-1]) 
-        else:
-            if np.ndim(self._values)==1:
-                return 'Length: {} [{}]'.format(len(self._values), ','.join([str(z) for z in zip(self._times, self._values)]))      
-            else:
-                return 'Length: {} [{}]'.format(len(self._values), ','.join([str(z) for z in  self._values]))                      
+        r = reprlib.Repr()
+        r.maxlist = 3       # max elements displayed for lists        
+        cls = type(self).__name__
+        timesStr  = r.repr(self._times)
+        timesStr = r.repr(self._values)
+        return "{} with {} elements (Times: {}, Values: {})".format(cls, len(self._values), timesStr, timesStr)                     
         
     def __iter__(self):           
         for i in self._values:
             yield i
             
+            
     def __itertimes__(self):        
         for i in self._times:
             yield i    
+            
             
     def __iteritems__(self):          
         for i,j in zip(self._times,self._values):
             yield i,j
     
+    
     @lazy.lazy
     def identity(self): 
         return self
+    
     
     @property
     def lazy(self):
         return self.identity()
     
+    
     def __pos__(self):
-        # Returns: TimeSeries instance with no change to the values or times
+        # returns: TimeSeries instance with no change to the values or times
         return self
         
+        
     def __neg__(self):
-        # Returns: TimeSeries instance with negated values and no change to the times
+        # returns: TimeSeries instance with negated values and no change to the times
         cls = type(self)
         return cls((-v for v in self._values), self._times)
         
+        
     def __abs__(self):
-        # Returns: new TimeSeries instance with absolute value of the values
+        # returns: new TimeSeries instance with absolute value of the values
         #     and no change to the times
         cls = type(self)
         return cls((abs(v) for v in self._values), self._times)
     
+    
     def __bool__(self):
-        # Returns: bool `False` iff all `_values` are zero
+        # returns: bool `False` iff all `_values` are zero
         return bool(any(self._values))
         
+        
     def __add__(self, rhs):
-        # If rhs is Real, add it to all elements of `_values`.
-        # If rhs is a TimeSeries instance with the same times, add it element-by-element.
-        # Returns: a new TimeSeries instance with the same times but updated `_values`.
+        # if rhs is Real, add it to all elements of `_values`.
+        # if rhs is a TimeSeries instance with the same times, add it element-by-element.
+        # returns: a new TimeSeries instance with the same times but updated `_values`.
         cls = type(self)
         try:
             if isinstance(rhs, numbers.Real):
@@ -172,19 +149,21 @@ class TimeSeries:
         except TypeError:
             return NotImplemented
             
+            
     def __sub__(self, rhs):
-        # If rhs is Real, subtract it from all elements of `_values`.
-        # If rhs is a TimeSeries instance with the same times, subtract it element-by-element.
-        # Returns: a new TimeSeries instance with the same times but updated `_values`.
+        # if rhs is Real, subtract it from all elements of `_values`.
+        # if rhs is a TimeSeries instance with the same times, subtract it element-by-element.
+        # returns: a new TimeSeries instance with the same times but updated `_values`.
         try:
             return self + (-rhs)
         except TypeError:
             return NotImplemented
     
+    
     def __mul__(self, rhs):
-        # If rhs is Real, multiply it by all elements of `_values`.
-        # If rhs is a TimeSeries instance with the same times, multiply it element-by-element.
-        # Returns: a new TimeSeries instance with the same times but updated `_values`.
+        # if rhs is Real, multiply it by all elements of `_values`.
+        # if rhs is a TimeSeries instance with the same times, multiply it element-by-element.
+        # returns: a new TimeSeries instance with the same times but updated `_values`.
         cls = type(self)
         try:
             if isinstance(rhs, numbers.Real):
@@ -199,20 +178,23 @@ class TimeSeries:
         except TypeError:
             return NotImplemented
     
+    
     def _eqtimes(self,rhs):
         # test equality of the time components of two TimeSeries instances
         return len(self._times)==len(rhs._times) and all(a==b for a,b in zip(self._times,rhs._times))
 
+    
     def _eqvalues(self,rhs):
-        # Test equality of the values components of two TimeSeries instances
+        # test equality of the values components of two TimeSeries instances
         return len(self._values)==len(rhs._values) and all(a==b for a,b in zip(self._values,rhs._values))
+        
         
     def __eq__(self,rhs):
         # True if the times and values are the same; otherwise, False
         cls = type(self)
         if isinstance(rhs, cls):
             return self._eqtimes(rhs) and self._eqvalues(rhs)
-        #elif isinstance(rhs, numbers.Real):
+        # elif isinstance(rhs, numbers.Real):
         #    return all(v==rhs for v in self._values)
         else:
             return False
