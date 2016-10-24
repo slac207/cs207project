7ecb8119cb68a0b3360affbf6ef6681fae0277f0
Courtney <cocochrane@davidson.edu>
2016-10-24 17:11:13 -0400

    #5 Fixed ArrayTimeSeries storage

--- timeseries/ArrayTimeSeries.py
+++ timeseries/ArrayTimeSeries.py
@@ -4,55 +4,70 @@ import numpy as np
 class ArrayTimeSeries(TimeSeries):
     """
     It is a new class ArrayTimeSeries which inherits from TimeSeries
-    class and uses numpy.array to store the data internally.
+    class and uses numpy arrays to store the data internally.
 
     Parameters
     ----------
-    values : a sequence
-    data used to populate time series instance.
-    times  : a sequence (optional)
-    time associated with each observation in `values`.
+    values : a sequence- data used to populate time series instance.
+    times  : a sequence- time associated with each observation in `values`.
 
-    >>> threes=range(0,1000,3)
-    >>> ap=ArrayTimeSeries(threes)
-    >>> ap[0]=999
-    >>> ap[0]
-    array([  0, 999])
-    >>> ap
-    ArrayTimeSeries(Length: 334, Times: range(0, 334), Values: array([[  0, ...  [333, 999]]))
-    >>> print(ap)
-    ArrayTimeSeries with 334 elements (Times: range(0, 334), Values: array([[  0, ...  [333, 999]]))
-
-    Now test non-uniform time series
-    >>> a = ArrayTimeSeries(values=[0,5,10,8,7], times=[1,2.5,3,3.5,4])
-    >>> a
-    ArrayTimeSeries(Length: 5, Times: [1, 2.5, 3, ...], Values: array([[  1. ... 4. ,   7. ]]))
-    >>> print(a)
-    ArrayTimeSeries with 5 elements (Times: [1, 2.5, 3, ...], Values: array([[  1. ... 4. ,   7. ]]))
+    >>>threes=range(0,1000,3)
+    >>>times = range(0,1000,1)
+    >>>ap=ArrayTimeSeries(times,threes)
+    >>>ap[0] = 999
+    >>>ap[0]
+    999
+    >>>ap
+    ArrayTimeSeries(Length: 334, Times: array([  0,  ...97, 998, 999]), Values: array([999,  ...93, 996, 999]))
+    >>>print(ap)
+    ArrayTimeSeries with 334 elements (Times: array([  0,  ...97, 998, 999]), Values: array([999,  ...93, 996, 999]))
+    >>>len(ap)
+    334
+    >>>ap = ap=ArrayTimeSeries([0,1,2,3],[5,6,7,8])
+    >>>list(iter(ap))
+    [5, 6, 7, 8]
+    >>>list(ap.itertimes())
+    [0, 1, 2, 3]
+    >>>list(ap.iteritems())
+    [(0, 5), (1, 6), (2, 7), (3, 8)]
+    
+    #Now test non-uniform time series
+    >>>a = ArrayTimeSeries(times=[1,2.5,3,3.5,4],values=[0,5,10,8,7], )
+    >>>a
+    ArrayTimeSeries(Length: 5, Times: array([ 1. , ...,  3.5,  4. ]), Values: array([ 0,  5, 10,  8,  7]))
+    >>>print(a)
+    ArrayTimeSeries with 5 elements (Times: array([ 1. , ...,  3.5,  4. ]), Values: array([ 0,  5, 10,  8,  7]))
 
     """
-    def __init__(self,values,times=None):
+    def __init__(self,times,values):
         TimeSeries.__init__(self,values,times)
-        self._values=np.column_stack((self._times, values))
+        self._times = np.array(times)
+        self._values = np.array(values)
 
-    def __getitem__(self,time):
+    def __getitem__(self,index):
         try:
-            return self._values[self._times.index(time),]
+            return self._values[index]
         except IndexError:
-            raise("Index out of bounds")
+            raise IndexError("Index out of bounds")
 
-    def __setitem__(self,time,value):
+    def __setitem__(self,index,value):
         try:
-            self._values[self._times.index(time),1] = value
+            self._values[index] = value
         except IndexError:
-            raise("Index out of bounds")
+            raise IndexError("Index out of bounds")
 
     def __len__(self):
-        return len(self._values)
+        return super().__len__()
 
+    def __iter__(self):
+        return super().__iter__()
+        
     def itertimes(self):
         return super().itertimes()
 
     def iteritems(self):
-        for i,j in self._values:
-            yield i,j
+        return super().iteritems()
+    
+    
+
+
