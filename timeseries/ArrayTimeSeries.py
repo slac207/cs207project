c5fad1ecf12ea65b71e1b974dd7650943e9edecd
Courtney <cocochrane@davidson.edu>
2016-10-26 19:21:07 -0400

    #35 Fixed TimeSeries constructor

--- timeseries/ArrayTimeSeries.py
+++ timeseries/ArrayTimeSeries.py
@@ -10,32 +10,7 @@ class ArrayTimeSeries(TimeSeries):
     values : a sequence- data used to populate time series instance.
     times  : a sequence- time associated with each observation in `values`.
 
-    >>> threes=range(0,1000,3)
-    >>> times = range(0,1000,1)
-    >>> ap=ArrayTimeSeries(times,threes)
-    >>> ap[0] = 999
-    >>> ap[0]
-    999
-    >>> repr(ap)
-    'ArrayTimeSeries(Length: 334, Times: array([  0,  ...97, 998, 999]), Values: array([999,  ...93, 996, 999]))'
-    >>> print(ap)
-    ArrayTimeSeries with 334 elements (Times: array([  0,  ...97, 998, 999]), Values: array([999,  ...93, 996, 999]))
-    >>> len(ap)
-    334
-    >>> ap = ap=ArrayTimeSeries([0,1,2,3],[5,6,7,8])
-    >>> list(iter(ap))
-    [5, 6, 7, 8]
-    >>> list(ap.itertimes())
-    [0, 1, 2, 3]
-    >>> list(ap.iteritems())
-    [(0, 5), (1, 6), (2, 7), (3, 8)]
-
-    #Now test non-uniform time series
-    >>> a = ArrayTimeSeries(times=[1,2.5,3,3.5,4],values=[0,5,10,8,7], )
-    >>> a
-    ArrayTimeSeries(Length: 5, Times: array([ 1. , ...,  3.5,  4. ]), Values: array([ 0,  5, 10,  8,  7]))
-    >>> print(a)
-    ArrayTimeSeries with 5 elements (Times: array([ 1. , ...,  3.5,  4. ]), Values: array([ 0,  5, 10,  8,  7]))
+   
 
     """
     def __init__(self,times,values):
@@ -68,13 +43,3 @@ class ArrayTimeSeries(TimeSeries):
         return super().iteritems()
 
 
-
-#threes=range(0,1000,3)
-#times = range(0,1000,1)
-#ap=ArrayTimeSeries(times,threes)
-#ap[0] = 999
-#ap[0]
-##999
-#print("HERE",repr(ap))
-##ArrayTimeSeries(Length: 334, Times: array([  0,  ...97, 998, 999]), Values: array([999,  ...93, 996, 999]))
-#print(ap)
