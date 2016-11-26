0c454d4b1525e0bc2dc87dc8a59eefb857d975cc
jsm09 <lware96@gmail.com>
2016-11-26 00:45:33 -0500

    #63 add init tests

--- cs207rbtree/test_RedBlackTree.py
+++ cs207rbtree/test_RedBlackTree.py
@@ -4,8 +4,7 @@ import numpy as np
 from RedBlackTree import *
 import os
 
-class TimeSeriesTest(unittest.TestCase):
-
+class RedBlackTreeTest(unittest.TestCase):
 
     def test_smoke(self):
         "smoke test to make sure unbalanced tree is working"
@@ -26,6 +25,20 @@ class TimeSeriesTest(unittest.TestCase):
         newdb = connect("/tmp/test2.dbdb")
         assert newdb.get("pavlos")=="aged"
         newdb.close()
+        os.remove('/tmp/test2.dbdb')
+
+    def test_inits(self):
+        "test init methods"
+        dbname = "/tmp/test2.dbdb"
+        try:
+            f = open(dbname, 'r+b')
+        except IOError:
+            fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
+            f = os.fdopen(fd, 'r+b')
+        storage = Storage(f)
+        binaryTree = BinaryTree(storage)
+        db = DBDB(f)
+        db.close()
         os.remove('/tmp/test2.dbdb')
 
 if __name__=='__main__':
