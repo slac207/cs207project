3275ec1faa05bf22af5cef371e8840af0f7c8dad
jsm09 <lware96@gmail.com>
2016-11-26 22:50:12 -0500

    troubleshoot coveralls issue

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@
 
 import sys
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 
 def setup_package():
@@ -19,54 +18,6 @@ def setup_package():
     setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
           use_pyscaffold=True)
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
 
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-
-setup(
-    name='coveralls',
-    version='1.1',
-    packages=['coveralls'],
-    url='http://github.com/coagulant/coveralls-python',
-    license='MIT',
-    author='Ilya Baryshev',
-    author_email='baryshev@gmail.com',
-    description='Show coverage stats online via coveralls.io',
-    long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read(),
-    entry_points={
-        'console_scripts': [
-            'coveralls = coveralls.cli:main',
-        ],
-    },
-    install_requires=['docopt>=0.6.1', 'coverage>=3.6', 'requests>=1.0.0'],
-    tests_require=['mock', 'pytest>=2.7.3,<2.8', 'sh>=1.08'],
-    extras_require={
-        'yaml': ['PyYAML>=3.10']
-    },
-    cmdclass={'test': PyTest},
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Software Development :: Testing',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Programming Language :: Python :: Implementation :: CPython',
-    ],
-)
 if __name__ == "__main__":
     setup_package()
