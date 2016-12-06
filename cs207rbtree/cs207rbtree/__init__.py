8b3ac4cea1cb61b7d32f23e4d14b54c46ef840d3
Sarah Anoke <sanoke527@gmail.com>
2016-12-06 06:42:30 -0500

    Trying to get Travis to import cs207rbtree correctly

--- cs207rbtree/cs207rbtree/__init__.py
+++ cs207rbtree/cs207rbtree/__init__.py
@@ -1,10 +1,1 @@
-import pkg_resources
-import sys
-sys.path.append('../cs207rbtree/')
-
 from cs207rbtree import *
-
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except:
-    __version__ = 'unknown'
