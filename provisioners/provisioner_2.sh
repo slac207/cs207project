ce3dbbbd5969852a23207a79390ec6d1cce4a730
Andrew Brennan <brennan.ae@gmail.com>
2016-12-12 11:52:34 -0500

    added provisioning scripts

---
+++ provisioners/provisioner_2.sh
@@ -0,0 +1,46 @@
+#mkdir ~/flaskproj
+#cd ~/flaskproj
+
+# # to ~/flaskproj/flaskproj.py:
+# # a Flask file, such as:
+# echo '''from flask import Flask
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+    # return "<h1 style='color:green'>Hello There!</h1>"
+
+# if __name__ == "__main__":
+    # app.run(host="0.0.0.0")''' > ~/cs207project/metadataflaskproj/flaskproj.py
+
+    
+# to ~/flaskproj/wsgi.py:
+echo '''from rest import app
+
+if __name__ == "__main__":
+    app.run()''' > ~/cs207project/metadata/wsgi.py
+
+
+#to ~/flaskproj/flaskproj.ini:
+echo '''[uwsgi]
+module = wsgi:app
+
+master = true
+processes = 5
+
+socket = flaskproj.sock
+chmod-socket = 660
+vacuum = true
+
+die-on-term = true''' > ~/cs207project/metadata/flaskproj.ini
+
+#sudo rm /etc/nginx/sites-available/default
+#sudo rm /etc/nginx/sites-enabled/default
+
+sudo systemctl start flaskproj
+sudo systemctl enable flaskproj
+
+sudo ln -s /etc/nginx/sites-available/flaskproj /etc/nginx/sites-enabled
+
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full' 
