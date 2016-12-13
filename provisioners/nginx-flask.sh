86d8d934ab5d70f00d64b8f912b62e7a37a3668d
Andrew Brennan <brennan.ae@gmail.com>
2016-12-13 11:02:51 -0500

    refactored and extended provisioning

---
+++ provisioners/nginx-flask.sh
@@ -0,0 +1,35 @@
+
+cd ~/cs207project/metadata
+
+printf "\n*******************************************************"
+print "\n Set up uWSGI"
+
+# Entry point for wsgi into flask
+cp ~/cs207project/provisioners/files/wsgi.py ./wsgi.py
+
+# uWSGI setup file
+cp ~/cs207project/provisioners/files/flaskproj.ini ./flaskproj.ini
+
+# Setup file for running uWSGI as a systemd process
+sudo cp ~/cs207project/provisioners/files/flaskproj.service /etc/systemd/system/flaskproj.service
+
+# Start flaskproj as a systemd process
+sudo systemctl start flaskproj
+sudo systemctl enable flaskproj
+
+# Check that wWSGI process is running properly
+sudo systemctl status flaskproj
+
+
+printf "\n*******************************************************"
+print "\n Set up nginx to communicate with uWSGI"
+
+# Remove the nginx default site
+sudo rm /etc/nginx/sites-enabled/default
+
+# Make a server file for routing requests based on URI
+sudo cp ~/cs207project/provisioners/files/flaskproj /etc/nginx/sites-available/flaskproj 
+sudo ln -s /etc/nginx/sites-available/flaskproj /etc/nginx/sites-enabled
+
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full' 
