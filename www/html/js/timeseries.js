5f203a953977c5b7e0f4ab74de8f65f5e0bc36c4
Sarah Anoke <sanoke527@gmail.com>
2016-12-14 10:15:43 -0500

    Rolling back timeseries.js

--- www/html/js/timeseries.js
+++ www/html/js/timeseries.js
@@ -190,7 +190,7 @@
 					console.log("Locally received data uploaded by user.");			
 					console.log( document.getElementById("fileSelect").files[0] );
 			
-					var simurl = publicIP + "/simquery?" + "id=" + "&topn=" + numSim;
+					var simurl = publicIP + "/simquery?id=" + "&topn=" + numSim;
 
 					console.log("Sending user-loaded data off to be processed:" + simurl);
 					$.ajax({
@@ -210,28 +210,34 @@
 						.done(function (result) {
 		
 							simIDs = result.id;
-							
-							// loop over all the IDs and collect their data
-							console.log("Looping over the retrieved ID numbers and collecting their data.");
-							
-							for(var i=0; i < simIDs.length; i++) {
 				
-								idNum   = simIDs[i];
-					
-								var dataurl = publicIP + "/timeseries/" + idNum;
-					
-								console.log('Retrieving data associated with TS ID#' + idNum + '.');
-								// solution from http://stackoverflow.com/questions/4201934/
-								//               jquery-ajax-pass-additional-argument-to-success-callback
-								$.ajax({
-									url: dataurl,
-									type: "GET",
-									dataType: "json",
-									success: onDataReceived
-								});
-				
-							}	
+							$.ajax({
+								url: simurl,
+								type: "GET",
+								dataType: "json",
+								success: function(data) {
 
+										// loop over all the IDs and collect their data
+										console.log("Looping over the retrieved ID numbers and collecting their data.");
+							
+										for(var i=0; i < simIDs.length; i++) {
+				
+											idNum   = simIDs[i];
+								
+											var dataurl = publicIP + "/timeseries/" + idNum;
+								
+											console.log('Retrieving data associated with TS ID#' + idNum + '.');
+											// solution from http://stackoverflow.com/questions/4201934/
+											//               jquery-ajax-pass-additional-argument-to-success-callback
+											$.ajax({
+												url: dataurl,
+												type: "GET",
+												dataType: "json",
+												success: onDataReceived
+											});
+				
+										}		
+							 }
 						});
 			
 					// parse the uploaded file
